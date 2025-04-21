9e55ab5aaa3a313a0203e1f246bf223e130555ee
yongyi <yongyi.ee13@nycu.edu.tw>
2025-04-21 19:29:47 +0800

    ahhhhhhhh

--- src/Cylinder.cpp
+++ src/Cylinder.cpp
@@ -2,32 +2,39 @@
 # define CYLINDER_CPP
 
 # include "Cylinder.h"
+# include <iomanip>
+
+const double pi = 3.142;
 
 double Cylinder :: SurfaceArea(){
-    
-    return ;
+    double circle = radius * radius * pi * 2;
+    double rectangle = 2.0 * radius * pi * height;
+    return (circle + rectangle);
 }
 
 double Cylinder :: Volume(){
-
-    return ;
+    double volume = radius * radius * pi * height;
+    return volume;
 }
 
 double Cylinder :: Circumference(){
-
-    return ;
+    double cir = radius * pi * 2.0 * 2.0;
+    return cir;
 }
 
 istream & operator>>(istream & in, Cylinder & cldr)
 {
-
-    return ;
+    in >> cldr.radius >> cldr.height;
+    return in;
 }
 
 ostream & operator<<(ostream & out, Cylinder & cldr)
 {
-    
-    return ;
+    out << std::fixed << std::setprecision(3);
+    out << "Circumference: " << cldr.Circumference() << endl;
+    out << "SurfaceArea: " << cldr.SurfaceArea() << endl;
+    out << "Volume: " << cldr.Volume() << endl;
+    return out;
 }
 
 # endif
